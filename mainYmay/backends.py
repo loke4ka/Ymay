f6a78605405f6f0d5194ea54d2736c54371e0d46
loke4ka <88316923+loke4ka@users.noreply.github.com>
2023-06-03 00:27:19 +0600

    Backends fixed for login and logout

--- mainYmay/backends.py
+++ mainYmay/backends.py
@@ -1,8 +1,17 @@
 from mainYmay.models import User
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.backends import ModelBackend
 
 
-class UserBackend:
-    def authenticate(self, request, email=None, password=None):
+class UserBackend(ModelBackend):
+    def logout(self, request):
+        # Очистка данных сессии
+        request.session.flush()
+
+        # Удаляем идентификатор сессии из cookie
+        auth_logout(request)
+
+    def authenticate(self, request, email=None, password=None, **kwargs):
         try:
             user = User.objects.get(email=email)
         except User.DoesNotExist:
@@ -12,6 +21,9 @@ class UserBackend:
             return user
 
     def get_user(self, user_id):
+        if user_id is None:
+            return None
+
         try:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
